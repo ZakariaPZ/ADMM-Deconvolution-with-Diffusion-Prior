8315aaa0bb927fa531c439a054e401d8100061e5
akasharidas <akash.haridas@gmail.com>
2022-11-30 14:49:27 -0500

    save results to file instead of plotting

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import argparse
 import skimage.io as io
 from skimage.metrics import peak_signal_noise_ratio as compute_psnr
 from skimage.filters import gaussian
+import os
 
 from pypher.pypher import psf2otf
 import matplotlib.pyplot as plt
@@ -32,10 +33,11 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
+    os.makedirs(f"./results", exist_ok=True)
+
     # Select image
     img = io.imread(f'testimgs\{args.img_name}').astype(float)/255
 
-    # img = img.tran(img.shape[1], img.shape[2], 3)
     # blur kernel
     c = fspecial_gaussian_2d((30, 30), 2.5)
 
@@ -71,12 +73,8 @@ if __name__ == '__main__':
             x_admm_tv[:, :, it] = deconv_admm_tv(b[:, :, it], c, lam, rho, num_iters)
         x_admm_tv = np.clip(x_admm_tv, 0.0, 1.0)
         PSNR_ADMM_TV = round(compute_psnr(img, x_admm_tv), 1)
+        plt.imsave(f"./results/admm_tv_psnr{round(PSNR_ADMM_TV, 1)}.png", x_admm_tv)
 
-        ax = fig.add_subplot(3, 2, 3)
-        ax.imshow(x_admm_tv)
-        ax.set_title("ADMM TV, PSNR: " + str(PSNR_ADMM_TV))
-        ax.xaxis.set_visible(False)
-        ax.yaxis.set_visible(False)
 
     if(args.wiener or all_condition):
         # # ADMM parameters for DnCNN  & Wiener Prior
@@ -90,12 +88,8 @@ if __name__ == '__main__':
             x_admm_wiener[:, :, it] = deconv_admm_wiener(b[:, :, it], c, lam, rho, num_iters, sigma)
         x_admm_wiener = np.clip(x_admm_wiener, 0.0, 1.0)
         PSNR_ADMM_WIENER = round(compute_psnr(img, x_admm_wiener), 1)
+        plt.imsave(f"./results/admm_wiener_psnr{round(PSNR_ADMM_WIENER, 1)}.png", x_admm_wiener)
 
-        ax = fig.add_subplot(3, 2, 4)
-        ax.imshow(x_admm_wiener)
-        ax.set_title("ADMM Wiener, PSNR: " + str(PSNR_ADMM_WIENER))
-        ax.xaxis.set_visible(False)
-        ax.yaxis.set_visible(False)
 
     if(args.dncnn or all_condition):
         # train DnCNN
@@ -109,12 +103,9 @@ if __name__ == '__main__':
         b = np.clip(b, 0, 1)
         img = np.clip(img, 0, 1)
         PSNR_ADMM_DNCNN = round(compute_psnr(img, x_admm_dncnn), 1)
+        plt.imsave(f"./results/admm_dncnn_psnr{round(PSNR_ADMM_DNCNN, 1)}.png", x_admm_dncnn)
 
-        ax = fig.add_subplot(3, 2, 3)
-        ax.imshow(x_admm_dncnn)
-        ax.set_title("ADMM DnCNN, PSNR: " + str(PSNR_ADMM_DNCNN))
-        ax.xaxis.set_visible(False)
-        ax.yaxis.set_visible(False)
+
 
     if(args.bilateral or all_condition):
         sigmaIntensity = 0.25
@@ -123,12 +114,9 @@ if __name__ == '__main__':
         for it in range(3):
             x_admm_bil[:, :, it] = deconv_admm_bilateral(b[:, :, it], c, lam, rho, num_iters, sigma, sigmaIntensity)
         PSNR_ADMM_BIL = round(compute_psnr(img, x_admm_bil), 1)
+        plt.imsave(f"./results/admm_bil_psnr{round(PSNR_ADMM_BIL, 1)}.png", x_admm_bil)
 
-        ax = fig.add_subplot(3, 2, 5)
-        ax.imshow(x_admm_bil)
-        ax.set_title("ADMM Bilateral, PSNR: " + str(PSNR_ADMM_BIL))
-        ax.xaxis.set_visible(False)
-        ax.yaxis.set_visible(False)
+
 
     if(args.nlm or all_condition):
         # # Set up NLM
@@ -144,23 +132,5 @@ if __name__ == '__main__':
         for it in range(3):
             x_admm_NLM[:, :, it] = deconv_admm_NLM(b[:, :, it], c, lam, rho, num_iters, searchWindowRadius, sigma, nlmSigma)
         PSNR_ADMM_NLM = round(compute_psnr(img, x_admm_NLM), 1)
+        plt.imsave(f"./results/admm_nlm_psnr{round(PSNR_ADMM_NLM, 1)}.png", x_admm_NLM)
 
-        ax = fig.add_subplot(3, 2, 6)
-        ax.imshow(x_admm_NLM)
-        ax.set_title("ADMM NLM, PSNR: " + str(PSNR_ADMM_NLM))
-        ax.xaxis.set_visible(False)
-        ax.yaxis.set_visible(False)
-
-    ax = fig.add_subplot(3, 2, 1)
-    ax.imshow(img)
-    ax.set_title("Target Image")
-    ax.xaxis.set_visible(False)
-    ax.yaxis.set_visible(False)
-
-    ax = fig.add_subplot(3, 2, 2)
-    ax.imshow(b)
-    ax.set_title("Blurry and Noisy Image")
-    ax.xaxis.set_visible(False)
-    ax.yaxis.set_visible(False)
-
-    plt.show()
